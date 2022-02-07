21e15ce9db6efb3a3299fd16256b6a13e1f291c3
Maxime Deryck <maxime.deryck@gmail.com>
2022-02-07 22:43:40 +0100

    Small rework

--- life_on_earth/main.py
+++ life_on_earth/main.py
@@ -27,7 +27,7 @@ DATA_FILE = BASE_FOLDER/'data.csv'
 
 # Set up Pi Camera
 CAMERA = PiCamera()
-CAMERA.resolution(4056,3040) # Approx. 6 MB / pic or 170 pics / GB
+CAMERA.resolution(4056, 3040) # Approx. 6 MB / pic or 170 pics / GB
 
 # CSV functions (from Astro Pi Phase 2 Guide)
 def create_csv(data_file, header):
@@ -54,40 +54,8 @@ def add_csv_data(data_file, data):
         writer = csv.writer(file)
         writer.writerow(data)
 
-# Camera functions (from Astro Pi Phase 2 Guide)
-def convert(angle):
-    """
-    Convert a `skyfield` Angle to an EXIF-appropriate
-    representation (rationals)
-    e.g. 98° 34' 58.7 to "98/1,34/1,587/10"
-
-    Return a tuple containing a boolean and the converted angle,
-    with the boolean indicating if the angle is negative.
-    """
-    sign, degrees, minutes, seconds = angle.signed_dms()
-    exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
-
-    return sign < 0, exif_angle
-
-def capture(camera, image):
-    """Use `camera` to capture an `image` file with lat/long EXIF data."""
-    point = ISS.coordinates()
-
-    # Convert the latitude and longitude to EXIF-appropriate representations
-    south, exif_latitude = convert(point.latitude)
-    west, exif_longitude = convert(point.longitude)
-
-    # Set the EXIF tags specifying the current location
-    camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
-    camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
-    camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
-    camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
-
-    # Capture the image
-    camera.capture(image)
-
 # Create CSV file
-create_csv(DATA_FILE, ("Date/Time", "Filename"))
+create_csv(DATA_FILE, ("Image", "Predicted cloud type"))
 
 # Record the current time
 current_time = datetime.now()
